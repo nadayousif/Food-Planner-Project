0183cdea651c077e3187e83833fe7d4bd0cd8318
nadayousif <78286387+nadayousif@users.noreply.github.com>
2023-02-16 03:20:16 +0200

    user class

--- Food Project/app/src/main/java/com/example/foodplanner/helper/MyUser.java
+++ Food Project/app/src/main/java/com/example/foodplanner/helper/MyUser.java
@@ -1,12 +1,30 @@
 package com.example.foodplanner.helper;
 
+
+
 public class MyUser {
 
-    public static MyUser getInstance() {
-        return null;
+    private String email;
+    private static MyUser instance = null;
+
+    private MyUser() {
+        this.email="";
+
+    }
+
+    public static synchronized MyUser getInstance() {
+        if (instance == null) {
+            instance = new MyUser();
+        }
+
+        return instance;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getEmail() {
-        return null;
+        return email;
     }
 }
